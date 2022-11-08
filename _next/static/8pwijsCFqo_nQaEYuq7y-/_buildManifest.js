13a55eccffdc82367175e90b9055e38b346272a7
n4gava <n4gava@users.noreply.github.com>
2022-11-08 13:07:08 +0000

    Build landing page

---
+++ _next/static/8pwijsCFqo_nQaEYuq7y-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/381-c1eadea01fd9d5d1.js","static/chunks/pages/index-068ba621aaa844ee.js"],"/_error":["static/chunks/pages/_error-d20dd478b791b37a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
